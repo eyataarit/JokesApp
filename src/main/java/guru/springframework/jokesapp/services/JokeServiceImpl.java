7133e808c84d7dac946a779192231ba6ba02891e
Aya Ben Taarit <taarit.aya@gmail.com>
2022-05-30 01:16:23 +0200

    Add Joke Service. closes #3

---
+++ src/main/java/guru/springframework/jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+        //this is hard coding, it's not a good practice.
+        // Not friendly for Dependency Injection. you better declare this as a bean
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
